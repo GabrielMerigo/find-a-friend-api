fe0024ae55c569743e05543c1e19c43dabaaf0fb
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-06-26 10:52:41 -0300

    more data

--- src/routes/pet.ts
+++ src/routes/pet.ts
@@ -1,6 +1,22 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 
 export const petRoutes = async (app: FastifyInstance) => {
-  app.post("/", () => {});
+  app.post("/", (request, response) => {
+    const petBodySchema = z.object({
+      age: z.enum(["CUB", "MIDDLE_AGE", "OLD"]),
+      name: z.string(),
+      color: z.string(),
+      small_size: z.enum(["SMALL", "MEDIUM", "BIG", "EXTRA_BIG"]),
+      city: z.string(),
+      energy_level: z.enum(["LOW", "MEDIUM", "HIGH"]),
+      independencie_level: z.enum(["LOW", "MEDIUM", "HIGH"]),
+      type: z.enum(["CAT", "DOG"]),
+      organization_id: z.string(),
+    });
+
+    const petBodyRequest = petBodySchema.parse(request.body);
+  });
+
   app.get("/:cityId", () => {});
 };
